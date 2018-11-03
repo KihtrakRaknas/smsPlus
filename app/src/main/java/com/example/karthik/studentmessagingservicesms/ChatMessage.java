e7663d7bfb67a6cc829ea60b2bd776cdff07c206
Sethu Senthil <sethunarayansenthil@gmail.com>
2018-11-03 15:51:26 -0400

    Slap Chat

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/ChatMessage.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/ChatMessage.java
@@ -1,12 +1,50 @@
 package com.example.karthik.studentmessagingservicesms;
 
-import java.util.Date;
+import android.os.Bundle;
 
-public class ChatMessage {
+import java.util.Date;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChatMessage extends AppCompatActivity{
 
     private String messageText;
     private String messageUser;
     private long messageTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab =
+                (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.input);
+
+                // Read the input field and push a new instance
+                // of ChatMessage to the Firebase database
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new ChatMessage(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                // Clear the input
+                input.setText("");
+            }
+        });
+
+    }
 
     public ChatMessage(String messageText, String messageUser) {
         this.messageText = messageText;
