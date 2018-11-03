f13b169b043a11b56e788788422fc89a6ff10565
KihtrakRaknas <karthikandsankar@gmail.com>
2018-11-03 15:18:37 -0400

    update

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/phoneORuser.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/phoneORuser.java
@@ -8,6 +8,6 @@ public class phoneORuser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.phoneORuser);
+        setContentView(R.layout.phoneoruser);
     }
 }
