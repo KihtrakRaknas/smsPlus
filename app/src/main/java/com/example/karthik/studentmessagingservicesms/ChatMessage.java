7ab42c9b22a6badaeb46a3a1ecb1f39230062b01
KihtrakRaknas <karthikandsankar@gmail.com>
2018-11-03 19:40:07 -0400

    update

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/ChatMessage.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/ChatMessage.java
@@ -6,17 +6,27 @@ import java.util.Date;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatMessage extends AppCompatActivity{
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef;
-
+    LinearLayout list;
+    int maxIndex=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +48,41 @@ public class ChatMessage extends AppCompatActivity{
                         FirebaseAuth.getInstance()
                                 .getCurrentUser()
                                 .getDisplayName());
-                myRef.setValue(mess);
+                myRef.child(""+maxIndex+1).setValue(mess);
 
                 // Clear the input
                 input.setText("");
+            }
+        });
+
+        list = findViewById(R.id.list_messages);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                list.removeAllViews();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    int index = Integer.parseInt(snapshot.getKey());
+                    if(index>maxIndex)
+                    maxIndex=index;
+                    indivMessage mess = snapshot.getValue(indivMessage.class);
+
+                    TextView message = new TextView(ChatMessage.this);
+                    //message.setLayoutParams(new LayoutParams());
+                    message.setText(mess.messageText);
+
+                    list.addView(message);
+                }
+                //String value = dataSnapshot.getValue(String.class);
+                //Log.d("READDATA", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("READDATA", "Failed to read value.", error.toException());
             }
         });
 
