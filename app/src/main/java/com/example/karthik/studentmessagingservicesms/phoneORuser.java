049e0068bb7add35baf1e2773b893371a69a6fb6
Yin4TheWin <yin4thewin@gmail.com>
2018-11-03 15:47:52 -0400

    ye

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/phoneORuser.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/phoneORuser.java
@@ -1,13 +1,47 @@
 package com.example.karthik.studentmessagingservicesms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class phoneORuser extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    String email;
+    String password;
+    Button next;
+    EditText mail;
+    EditText word;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phoneoruser);
+        next=findViewById(R.id.next);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mail=findViewById(R.id.mail);
+        mail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                email=mail.getText().toString();
+            }
+        });
     }
 }
