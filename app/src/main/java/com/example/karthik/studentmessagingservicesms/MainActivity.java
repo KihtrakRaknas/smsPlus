ed09f9257b4dfe28c871447c8d8c42629244e325
KihtrakRaknas <karthikandsankar@gmail.com>
2018-11-03 16:48:59 -0400

    update

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/MainActivity.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    Intent intent;
+    Intent intentSignedOut;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intent = new Intent(this,landing.class);
-        startActivity(intent);
+        intentSignedOut = new Intent(this,landing.class);
+        text = findViewById(R.id.id_text);
+        startActivity(intentSignedOut);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        text.setText(auth.getUid());
     }
 
     public void signUp(View v){
