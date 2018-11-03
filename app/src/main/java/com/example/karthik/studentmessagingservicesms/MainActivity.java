b93c7145c2d9c68bcf0a5fbbd7f6e67e6ea04fba
Yin4TheWin <yin4thewin@gmail.com>
2018-11-03 14:48:40 -0400

    added layouts

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/MainActivity.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/MainActivity.java
@@ -1,18 +1,104 @@
 package com.example.karthik.studentmessagingservicesms;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+    private Intent intent;
+    EditText mail;
+    EditText word;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        intent=new Intent(this, SchoolActivity.class);
+        mail=findViewById(R.id.mail);
+        word=findViewById(R.id.word);
+        mail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                email=mail.getText().toString();
+            }
+        });
+        word.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                password=word.getText().toString();
+            }
+        });
     }
 
-    public void signUp(View v){
+    public void onClick(View v){
+        logUser();
+    }
+    public void signUp(){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(intent);
+                        } else {
+                            // Fail
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
+    }
+    public void logUser() {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            startActivity(intent);
+                        } else {
+                            signUp();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
