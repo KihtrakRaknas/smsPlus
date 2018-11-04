23cd4a9c141bd51a6304dcc5b8b3d099737a0124
KihtrakRaknas <karthikandsankar@gmail.com>
2018-11-04 09:22:29 -0500

    Update

--- app/src/main/java/com/example/karthik/studentmessagingservicesms/chat.java
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/chat.java
@@ -1,14 +1,14 @@
 package com.example.karthik.studentmessagingservicesms;
 
 public class chat {
-    public String name1;
+    public String name;
     public String period;
     public String block;
     public chat(){
 
     }
     public chat(String name,String period,String block){
-        name1=name;
+        this.name=name;
         this.period = period;
         this.block = block;
 
