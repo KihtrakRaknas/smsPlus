dcf764d77ad1f077f4962e312f47e8f60509fabc
KihtrakRaknas <karthikandsankar@gmail.com>
2018-11-04 09:18:58 -0500

    Update

---
+++ app/src/main/java/com/example/karthik/studentmessagingservicesms/chat.java
@@ -0,0 +1,16 @@
+package com.example.karthik.studentmessagingservicesms;
+
+public class chat {
+    public String name1;
+    public String period;
+    public String block;
+    public chat(){
+
+    }
+    public chat(String name,String period,String block){
+        name1=name;
+        this.period = period;
+        this.block = block;
+
+    }
+}
